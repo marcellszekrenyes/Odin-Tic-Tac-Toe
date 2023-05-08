315f0c90117d918602a5f46e5c9c308c8ea35919
marcellszekrenyes <szekrenyesmarcell2@gmail.com>
2023-05-08 20:07:47 +0200

    javascript.js update with addId() function

--- javascript.js
+++ javascript.js
@@ -4,17 +4,20 @@
     const playBoard = document.querySelector('#playBoard');
     const boardSizer = document.querySelector('#boardSizer');
     const column = document.querySelector('.column');
+    
 
-    //controls #playBoard size changes
+    //controls #playBoard size changes -> it will be changed to start button
     boardSizer.addEventListener('click', ()=>{
-        playBoardStyler(setPlayBoardSize());
+        const choosenSize = boardSizer.value;
+        playBoardStyler(choosenSize);
+        setPlayBoardSize(choosenSize)
+        addId(choosenSize);
     })
     
     //sets size of #playBoard
-    function setPlayBoardSize() {
+    function setPlayBoardSize(choosenSize) {
         const columnCount = document.querySelector('#playBoard').childElementCount;
-        const choosenSize = boardSizer.value;
-
+        
         if(columnCount < Math.pow(choosenSize, 2)) {
             for (let i = columnCount; i < Math.pow(choosenSize, 2); i++) {
                 addColumn();
@@ -24,8 +27,6 @@
                 deleteColumn();
             }
         }
-        
-        return choosenSize;
     }
 
     //adds column at end of #playBoard
@@ -43,6 +44,23 @@
     function playBoardStyler(choosenSize) {
         playBoard.setAttribute('style', `grid-template-columns: repeat(${choosenSize}, 1fr); grid-template-rows: repeat(${choosenSize}, 1fr)`);
     }
+
+    //adds numbered id-s for all the columns
+    function addId(choosenSize) {
+        var allColumns = playBoard.children;
+        let i = 1;
+        let j = 1;
+        for (nextColumn of allColumns) { 
+            nextColumn.setAttribute('id', `column_${i}_${j}`);
+            j++;
+
+            if (j > choosenSize) {
+                i++;
+                j = 1;
+            }
+        }
+    }
+
 })();
 
 
